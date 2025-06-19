77bad7ef6c60cdb40db46102ebfcd5f9aaaeb856
Andx <andx667@protonmail.com>
2025-06-20 01:44:04 +0200

    fix more tabs and braces

--- addons/items/CfgVehicles.hpp
+++ addons/items/CfgVehicles.hpp
@@ -6,8 +6,8 @@ class CfgVehicles {
         displayName = CSTRING(box_displayName);
         model = "\A3\weapons_F\AmmoBoxes\WpnsBox_F";
         icon = "iconCrateWpns";
-        class TransportWeapons{};
-        class TransportMagazines{};
+        class TransportWeapons {};
+        class TransportMagazines {};
         class TransportItems {
             MACRO_ADDITEM(GVAR(US_Facepaint),30);
             MACRO_ADDITEM(GVAR(BW_Facepaint),30);
